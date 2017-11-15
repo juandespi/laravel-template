ac258126859d8954bb3f87092ce9feb858e10f89
John S. Despi <chattalogy@gmail.com>
2017-11-15 17:10:02 +0800

    semi charmed kinda life

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,43 +1,24 @@
 <!doctype html>
 <html lang="{{ app()->getLocale() }}">
-    <head>
-        <meta charset="utf-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+<head>
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Laravel</title>
-        <meta name="description" content="">
+  <title>Laravel</title>
+  <meta name="description" content="">
 
-        <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
-
-
-        <!-- Styles -->
-        <link rel="stylesheet" href="/css/app.css">
-
-    </head>
-    <body>
-      <header class="header--container">
-        <nav class="navlinks navlinks__left">
-          <a href=""><span>About Us</span></a>
-          <a href=""><span>Getting Started</span></a>
+  <!-- Fonts -->
+  <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
 
 
-          <a href=""></a>
-          <span></span>
-        </nav>
-      <div class="header--logo"></div>
-      <nav class="navlinks navlinks__right">
-        <a href=""><span>WOD</span></a>
-        <a href=""><span>Schedule</span></a>
-        <a href=""><span>Coaches</span></a>
-        <a href=""><span>Contact us</span></a>
-      </nav>
-      </header>
+  <!-- Styles -->
+  <link rel="stylesheet" href="/css/app.css">
 
-      <div class="box">
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Excepturi neque, officiis obcaecati? Explicabo, esse repellat dolorum accusamus optio deleniti eum, error modi omnis asperiores ipsa! Quia omnis repellat, obcaecati. Veniam?
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Excepturi neque, officiis obcaecati? Explicabo, esse repellat dolorum accusamus optio deleniti eum, error modi omnis asperiores ipsa! Quia omnis repellat, obcaecati. Veniam?
-      </div>
-    </body>
+</head>
+<body>
+  <div class="box">
+    <div class="box--inner"></div>
+  </div>
+</body>
 </html>
